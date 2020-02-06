5db1b2e2552eadd77c51b072570063184187df3b
karobest3 <karobest3@gmail.com>
2020-02-06 17:05:57 +0700

    's'

---
+++ src/app/word/word.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-word',
+    templateUrl: './word.component.html',
+    styleUrls: ['./word.component.scss']
+})
+export class WordComponent {
+    en = 'hello';
+    vn = 'xin chao';
+}
